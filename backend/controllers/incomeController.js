477850c9d1c681bbdb6cb76d3bfbc55f0173a38e
8qii <138139361+8qii@users.noreply.github.com>
2025-10-07 20:21:14 +0700

    update expense api

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,6 +1,6 @@
+const xlsx = require('xlsx');
 
-const User = require("../models/User")
-const Income = require("../models/Income")
+const Income = require("../models/Income");
 
 // add income source
 exports.addIncome = async (req, res) => {
@@ -27,14 +27,49 @@ exports.addIncome = async (req, res) => {
     } catch (error) {
         res.status(500).json({message: "Server Error"});
     }
-}
+};
 
 // get all income
-exports.getAllIncome = async (req, res) => {}
+exports.getAllIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try{
+        const income = await Income.find( {userId} ).sort( {date: -1} );
+        res.json(income);
+    } catch (error) {
+        res.status(500).json({message: "Server Error"});
+    }
+};
 
 // delete income
-exports.deleteIncome = async (req, res) => {}
+exports.deleteIncome = async (req, res) => {
+    try{
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({message: "Income deleted successfully"})
+    } catch (error){
+        res.status(500).json({message: "Server Error"})
+    }
+};
 
 // download income excel
-exports.downloadIncomeExcel = async (req, res) => {}
+exports.downloadIncomeExcel = async (req, res) => {
+    const userId = req.user.id;
 
+    try{
+        const income = await Income.find( {userId} ).sort( {date: -1} );
+        // prepare date for Excel
+        const data = income.map((item) => ({
+            Source: item.source,
+            Amount: item.amount,
+            Date: item.date,
+        }));
+
+        const wb = xlsx.utils.book_new();
+        const ws = xlsx.utils.json_to_sheet(data);
+        xlsx.utils.book_append_sheet(wb, ws, "Income");
+        xlsx.writeFile(wb, 'income_details.xlsx');
+        res.download('income_details.xlsx');
+    } catch (error){
+        res.status(500).json({message: "Server Error"})
+    }
+}
